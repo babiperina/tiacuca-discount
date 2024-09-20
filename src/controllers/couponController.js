33806111acc6fe45cb20b9570b3a1b10ed529433
Barbara Perina Bezerra <babiperina@MacBook-Air-de-Barbara.local>
2024-09-20 19:29:34 -0300

    update controller coupon

--- src/controllers/couponController.js
+++ src/controllers/couponController.js
@@ -1,11 +1,35 @@
 const { client } = require('../config/db');
+const { v4: uuidv4 } = require('uuid');  // Importa a função para gerar o uuid
+
+// Função para gerar um código de cupom único
+const generateUniqueCouponCode = async (collection) => {
+  let isUnique = false;
+  let coupon_code = '';
+
+  while (!isUnique) {
+    // Gera um UUID e limita para 22 caracteres
+    const uuid = uuidv4().replace(/-/g, '').substring(0, 22);
+    coupon_code = `MUVUKA-${uuid}`;
+
+    // Verifica se o código já existe na coleção
+    const existingCoupon = await collection.findOne({ coupon_code });
+    if (!existingCoupon) {
+      isUnique = true;  // Se o código não existir, é único
+    }
+  }
+
+  return coupon_code;
+};
 
 // Criar um novo cupom
 const createCoupon = async (req, res) => {
   try {
-    const { coupon_code, discount, expiration_date } = req.body;
+    const { discount, expiration_date } = req.body;
     const db = client.db('coupons_db');  // Nome do banco de dados
     const collection = db.collection('coupons');  // Nome da coleção
+
+    // Gera um código de cupom único
+    const coupon_code = await generateUniqueCouponCode(collection);
 
     const newCoupon = {
       coupon_code,
@@ -21,6 +45,7 @@ const createCoupon = async (req, res) => {
     res.status(400).json({ error: 'Erro ao criar o cupom' });
   }
 };
+
 
 // Buscar um cupom pelo código
 const getCoupon = async (req, res) => {
@@ -64,16 +89,16 @@ const useCoupon = async (req, res) => {
 
 // Retornar todos os cupons
 const getAllCoupons = async (req, res) => {
-    try {
-      const db = client.db('coupons_db');
-      const collection = db.collection('coupons');
-  
-      const coupons = await collection.find({}).toArray();  // Busca todos os documentos
-      res.json(coupons);  // Retorna a lista de cupons
-    } catch (error) {
-      res.status(500).json({ error: 'Erro ao buscar todos os cupons' });
-    }
-  };
+  try {
+    const db = client.db('coupons_db');
+    const collection = db.collection('coupons');
+
+    const coupons = await collection.find({}).toArray();  // Busca todos os documentos
+    res.json(coupons);  // Retorna a lista de cupons
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar todos os cupons' });
+  }
+};
 
 // Exports dos controladores
 module.exports = {
