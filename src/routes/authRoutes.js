14c548017bcb1ed94cd13ac016137559673f1e00
Barbara Perina Bezerra <babiperina@MacBook-Air-de-Barbara.local>
2024-09-29 20:03:50 -0300

    users get

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController'); // Importa o controlador
+const { registerUser, loginUser, getUsers } = require('../controllers/authController'); // Importa o controlador
 
 // Rota para registro de usuário
 router.post('/register', registerUser);
 
 // Rota para login de usuário
 router.post('/login', loginUser);
+
+// Rota para login de usuário
+router.get('/users', getUsers);
+
 
 module.exports = router;
